fe3300c1675b14458e63f67e8dacdafaec0684a8
ares <qq79402005@gmail.com>
2017-12-26 12:16:56 +0800

    文档补充。

--- server/start_server.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.system("(node websocket-relay.js 10001 10002 &)")
--- server/stop_server.py
+++
@@ -1,9 +0,0 @@
-import os
-import psutil
-
-cmdline_name = ['node', 'websocket-relay.js', '10001', '10002']
-
-for proc in psutil.process_iter():
-    if proc.cmdline() == cmdline_name:
-        print('stop server [kill -15 %d]' % proc.pid)
-        os.system('kill -15 %d' % proc.pid)
